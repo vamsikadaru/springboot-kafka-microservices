d28966ab4c2114f6f226dcd456b783f9c7548070
vamsi-git-test <vamsikadaru01@gmail.com>
2025-01-30 15:11:04 +0530

    Created dto classed

---
+++ base-domains/src/main/java/net/vamsi/basedomains/dto/Order.java
@@ -0,0 +1,15 @@
+package net.vamsi.basedomains.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    private String orderId;
+    private String orderName;
+    private String qty;
+    private double price;
+}
